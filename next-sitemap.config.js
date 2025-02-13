874f504238c6ec9ef5ef9ba988bb432badd5771f
michealabidfekrysaad <michealabidfekry@gmail.com>
2025-02-13 12:44:47 +0200

    add website url to sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "https://yourwebsite.com", // Replace with your actual domain
+  siteUrl: "https://shop.atlobha.com/", // Replace with your actual domain
   generateRobotsTxt: true, // Generates a robots.txt file
   sitemapSize: 5000, // Default size limit for the sitemap
   transform: async (config, url) => {
