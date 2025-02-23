cd0311c51d63973b9ec9872b49c3ae6bfdd705aa
michealabidfekrysaad <michealabidfekry@gmail.com>
2025-02-23 15:37:18 +0200

    serch  for the bug to fix

--- components/userProfile/orderDetails/sparePartsOrderDetails.js
+++ components/userProfile/orderDetails/sparePartsOrderDetails.js
@@ -104,54 +104,54 @@ function SparePartsOrderDetails({
     },
   });
 
-  useEffect(() => {
-    if (orderDetails?.id && router?.asPath && window?.webengage) {
-      if (type === ORDERSENUM?.spareParts) {
-        window.webengage.onReady(() => {
-          webengage.track("ORDER_SPAREPARTS_VIEWED", {
-            car_brand: orderDetails?.vehicle?.brand?.name || "",
-            car_model: orderDetails?.vehicle?.model?.name || "",
-            car_year: orderDetails?.vehicle?.year || "",
-            order_items:
-              orderDetails?.parts?.map((part) => ({
-                Part_Name_or_Number: part?.name || part?.id || "",
-                Quantity: part?.quantity || 0,
-                Image: part?.image || "",
-              })) || [],
-            shipping_address: orderDetails?.address?.address || "",
-            promo_code: orderDetails?.promo_code?.code || "",
-            comment: orderDetails?.notes || "",
-            order_number: orderDetails?.id || "",
-            creation_date: orderDetails?.created_at || "",
-            status: orderDetails?.status || "",
-            order_url: router?.asPath || "",
-          });
-        });
-      }
-      /* -------------------------------------------------------------------------- */
-      /*                           order viewed webengege                           */
-      /* -------------------------------------------------------------------------- */
-      window.webengage.onReady(() => {
-        webengage.track("ORDER_VIEWED", {
-          order_number: orderDetails?.id || "",
-          creation_date: orderDetails?.created_at || "",
-          order_items:
-            orderDetails?.parts?.map((part) => ({
-              Part_Name_or_Number: part?.name || part?.id || "",
-              Quantity: part?.quantity || 0,
-              Image: part?.image || "",
-            })) || [],
-          shipping_address: orderDetails?.address?.address || "",
-          deleivery_date: orderDetails?.estimated_delivery_date || "",
-          payment: orderDetails?.payment_method || "",
-          total_price: orderDetails?.receipt?.total_price || 0,
-          status: orderDetails?.status || "",
-          order_type: orderEnumArray()?.find((d) => d?.id === type)?.id || "",
-          order_url: router?.asPath || "",
-        });
-      });
-    }
-  }, [orderDetails?.id, router]);
+//   useEffect(() => {
+//     if (orderDetails?.id && router?.asPath && window?.webengage) {
+//       if (type === ORDERSENUM?.spareParts) {
+//         window.webengage.onReady(() => {
+//           webengage.track("ORDER_SPAREPARTS_VIEWED", {
+//             car_brand: orderDetails?.vehicle?.brand?.name || "",
+//             car_model: orderDetails?.vehicle?.model?.name || "",
+//             car_year: orderDetails?.vehicle?.year || "",
+//             order_items:
+//               orderDetails?.parts?.map((part) => ({
+//                 Part_Name_or_Number: part?.name || part?.id || "",
+//                 Quantity: part?.quantity || 0,
+//                 Image: part?.image || "",
+//               })) || [],
+//             shipping_address: orderDetails?.address?.address || "",
+//             promo_code: orderDetails?.promo_code?.code || "",
+//             comment: orderDetails?.notes || "",
+//             order_number: orderDetails?.id || "",
+//             creation_date: orderDetails?.created_at || "",
+//             status: orderDetails?.status || "",
+//             order_url: router?.asPath || "",
+//           });
+//         });
+//       }
+//       /* -------------------------------------------------------------------------- */
+//       /*                           order viewed webengege                           */
+//       /* -------------------------------------------------------------------------- */
+//       window.webengage.onReady(() => {
+//         webengage.track("ORDER_VIEWED", {
+//           order_number: orderDetails?.id || "",
+//           creation_date: orderDetails?.created_at || "",
+//           order_items:
+//             orderDetails?.parts?.map((part) => ({
+//               Part_Name_or_Number: part?.name || part?.id || "",
+//               Quantity: part?.quantity || 0,
+//               Image: part?.image || "",
+//             })) || [],
+//           shipping_address: orderDetails?.address?.address || "",
+//           deleivery_date: orderDetails?.estimated_delivery_date || "",
+//           payment: orderDetails?.payment_method || "",
+//           total_price: orderDetails?.receipt?.total_price || 0,
+//           status: orderDetails?.status || "",
+//           order_type: orderEnumArray()?.find((d) => d?.id === type)?.id || "",
+//           order_url: router?.asPath || "",
+//         });
+//       });
+//     }
+//   }, [orderDetails?.id, router]);
 
   return (
     <>
