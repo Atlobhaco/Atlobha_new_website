266c982c825b7f09514b4f2044f296554fc08fb2
michealabidfekrysaad <michealabidfekry@gmail.com>
2025-03-09 13:35:46 +0200

    allow corrs erorros

--- components/userProfile/orderDetails/summaryOrder.js
+++ components/userProfile/orderDetails/summaryOrder.js
@@ -592,6 +592,9 @@ function SummaryOrder({
           method: "POST",
           headers: {
             "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+            "Access-Control-Allow-Headers": "Content-Type, Authorization",
           },
           body: JSON.stringify(requestBody),
         });
