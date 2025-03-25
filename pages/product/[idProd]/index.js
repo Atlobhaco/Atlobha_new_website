94efd70ce177d4dfad7be9bbbd36e6a580969a45
michealabidfekrysaad <michealabidfekry@gmail.com>
2025-03-25 17:56:54 +0200

    add  new page  for product

---
+++ pages/product/[idProd]/index.js
@@ -0,0 +1,15 @@
+import MetaTags from "@/components/shared/MetaTags";
+import { Box } from "@mui/material";
+import React from "react";
+
+function ProductDetails() {
+  return (
+    <Box>
+      <MetaTags title={"product"} content={"product"} />
+
+      <ComingSoon />
+    </Box>
+  );
+}
+
+export default ProductDetails;
