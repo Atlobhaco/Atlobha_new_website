507180498d701d09eb695c24cf6637fff5fd77a8
michealabidfekrysaad <michealabidfekry@gmail.com>
2025-06-17 00:08:01 +0300

    remove require

--- postcss.config.js
+++ postcss.config.js
@@ -1,16 +1,20 @@
+// postcss.config.js
+import autoprefixer from "autoprefixer";
 import purgecss from "@fullhuman/postcss-purgecss";
 
-const isProd = true;
+const isProd = process.env.NODE_ENV === "production";
 
-export const plugins = [
-  require("autoprefixer"),
-  isProd &&
-    new purgecss({
-      content: [
-        "./pages/**/*.{js,jsx,ts,tsx}",
-        "./components/**/*.{js,jsx,ts,tsx}",
-        "./app/**/*.{js,jsx,ts,tsx}", // optional if you're using /app directory
-      ],
-      defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-    }),
-].filter(Boolean);
+export default {
+  plugins: [
+    autoprefixer,
+    isProd &&
+      purgecss({
+        content: [
+          "./pages/**/*.{js,jsx,ts,tsx}",
+          "./components/**/*.{js,jsx,ts,tsx}",
+          "./app/**/*.{js,jsx,ts,tsx}",
+        ],
+        defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+      }),
+  ].filter(Boolean),
+};
