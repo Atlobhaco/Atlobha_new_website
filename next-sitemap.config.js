eddb620c532b7fff0013f8f8a9646708f54d1ed3
michealabidfekrysaad <michealabidfekry@gmail.com>
2025-06-22 15:39:59 +0300

    add some  static urls inside site map

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -6,4 +6,25 @@ module.exports = {
   changefreq: "daily",
   priority: 0.7,
   exclude: ["/admin"], // optional paths to exclude
+  // Add your static pages manually
+  additionalPaths: async (config) => {
+    return [
+      { loc: "/userProfile/returnPolicy", lastmod: new Date().toISOString() },
+      { loc: "/userProfile/myCars", lastmod: new Date().toISOString() },
+      { loc: "/userProfile/myAddresses", lastmod: new Date().toISOString() },
+      { loc: "/userProfile/myOrders", lastmod: new Date().toISOString() },
+      { loc: "/userProfile/editInfo", lastmod: new Date().toISOString() },
+      { loc: "/sections?secTitle=", lastmod: new Date().toISOString() },
+      { loc: "/spareParts", lastmod: new Date().toISOString() },
+      { loc: "/category", lastmod: new Date().toISOString() },
+      { loc: "/category/4?idSub=3", lastmod: new Date().toISOString() },
+      { loc: "/product", lastmod: new Date().toISOString() },
+      { loc: "/product/2477937", lastmod: new Date().toISOString() },
+      { loc: "/manufacture", lastmod: new Date().toISOString() },
+      { loc: "/manufacture/2?name=بيزوس", lastmod: new Date().toISOString() },
+      { loc: "/checkout", lastmod: new Date().toISOString() },
+      { loc: "/spareParts/confirmation/", lastmod: new Date().toISOString() },
+      // Add more static routes here
+    ];
+  },
 };
