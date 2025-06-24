0af4591ec354cf9edd3b981f337695b46c543f8a
michealabidfekrysaad <michealabidfekry@gmail.com>
2025-06-24 18:17:03 +0300

    add cutom 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,5 +1,6 @@
+import ComingSoon from "@/components/comingSoon";
+
 // pages/404.js
 export default function Custom404() {
-	return <h1>404 - Page Not Found</h1>;
-  }
-  
+  return <ComingSoon />;
+}
