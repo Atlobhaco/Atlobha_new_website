c81e36a25bda30d5a0a7eaac2bf3f475508f066f
michealabidfekrysaad <michealabidfekry@gmail.com>
2025-07-15 14:21:57 +0300

    handle gif image in carousel and packages

--- components/Marketplace/MainCarousel/index.js
+++ components/Marketplace/MainCarousel/index.js
@@ -1,6 +1,5 @@
 import { Box } from "@mui/material";
 import React, { useState } from "react";
-import Slider from "react-slick";
 import { ADS } from "@/config/endPoints/endPoints";
 import useCustomQuery from "@/config/network/Apiconfig";
 import useScreenSize from "@/constants/screenSize/useScreenSize";
@@ -30,48 +29,6 @@ function MainCarousel({ sectionInfo }) {
       : false,
     select: (res) => res?.data,
   });
-
-  var settings = {
-    dots: true,
-    infinite: true,
-    slidesToShow: ads?.data?.length > 1 ? 1 : 1,
-    slidesToScroll: 1,
-    fade: true,
-    // autoplay: true,
-    // rtl: locale === "ar",
-    // touchThreshold: 10,
-    // speed: 6000,
-    // autoplaySpeed: 2000,
-    // cssEase: "linear",
-    // isDragging: false,
-    // initialSlide: 1,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-    ],
-    beforeChange: () => setIsDragging(true), // Set dragging to true when slide changes
-    afterChange: () => setTimeout(() => setIsDragging(false), 100), // Reset dragging state
-  };
-
   return !sectionInfo?.is_active || !ads?.data?.length ? null : (
     <Box>
       <Swiper
@@ -122,6 +79,7 @@ function MainCarousel({ sectionInfo }) {
                 display: "block",
                 margin: "auto",
                 cursor: img?.link ? "pointer" : "default",
+                maxHeight: isMobile ? "150px" : "612px",
               }}
               onClick={() => {
                 if (img?.link) {
