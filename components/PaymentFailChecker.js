36d885fb0a1ff394b5ec281fe33c160ab86065d2
michealabidfekrysaad <michealabidfekry@gmail.com>
2025-10-07 15:56:55 +0300

    render pay fail check when payment key changed

--- components/PaymentFailChecker.js
+++ components/PaymentFailChecker.js
@@ -8,47 +8,51 @@ import { ORDERS, PAYMENT_FAILED } from "@/config/endPoints/endPoints";
 // it saved in cookies after any payment failed escept MIS pay different flow
 export default function PaymentFailChecker() {
   const hasRun = useRef(false);
+  const paymentFailed = Cookies.get("payment_failed");
 
   useEffect(() => {
-    // if (hasRun.current) return; // prevent double execution
-    // hasRun.current = true;
+    if (paymentFailed) {
+      // if (hasRun.current) return; // prevent double execution
+      // hasRun.current = true;
 
-    const paymentFailed = Cookies.get("payment_failed");
-    const orderId = Cookies.get("created_order_id");
-    const orderType = Cookies.get("order_type");
-    const paytmentMethod = Cookies.get("payment_method");
-    const url_after_pay_failed = Cookies.get("url_after_pay_failed");
-    console.log(paymentFailed, orderId);
-    if (paymentFailed === "failed" && orderId) {
-      fetch(
-        `${process.env.NEXT_PUBLIC_API_BASE_URL}/${orderType}${ORDERS}/${orderId}${PAYMENT_FAILED}`,
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-            "x-api-key": "w123",
-            Authorization: `Bearer ${localStorage?.getItem("access_token")}`,
-          },
-        }
-      )
-        .then((res) => {
-          if (!res.ok) throw new Error("Request failed");
-          console.log(
-            "Payment fail (success) status updated for order:",
-            orderId
-          );
-        })
-        .catch((err) => console.error(err))
-        .finally(() => {
-          Cookies.remove("created_order_id");
-          Cookies.remove("payment_failed");
-          Cookies.remove("order_type");
-          Cookies.remove("payment_method");
-          Cookies.remove("url_after_pay_failed");
-        });
+      const orderId = Cookies.get("created_order_id");
+      const orderType = Cookies.get("order_type");
+      const paytmentMethod = Cookies.get("payment_method");
+      const url_after_pay_failed = Cookies.get("url_after_pay_failed");
+
+      console.log(paymentFailed, orderId);
+
+      if (paymentFailed === "failed" && orderId) {
+        fetch(
+          `${process.env.NEXT_PUBLIC_API_BASE_URL}/${orderType}${ORDERS}/${orderId}${PAYMENT_FAILED}`,
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+              "x-api-key": "w123",
+              Authorization: `Bearer ${localStorage?.getItem("access_token")}`,
+            },
+          }
+        )
+          .then((res) => {
+            if (!res.ok) throw new Error("Request failed");
+            console.log(
+              "Payment fail (success) status updated for order:",
+              orderId
+            );
+          })
+          .catch((err) => console.error(err))
+          .finally(() => {
+            Cookies.remove("created_order_id");
+            Cookies.remove("payment_failed");
+            Cookies.remove("order_type");
+            Cookies.remove("payment_method");
+            Cookies.remove("url_after_pay_failed");
+          });
+      }
+      // hasRun.current = false;
     }
-    // hasRun.current = false;
-  }, []);
+  }, [paymentFailed]);
 
   return null;
 }
