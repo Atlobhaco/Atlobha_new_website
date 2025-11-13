65185135b28caada41183bbe1e70c02937a1e571
michealabidfekrysaad <michealabidfekry@gmail.com>
2025-11-13 20:03:12 +0200

    add payment fail checker to the chekout for basket items

--- pages/checkout/CheckoutSummary.js
+++ pages/checkout/CheckoutSummary.js
@@ -709,7 +709,7 @@ const CheckoutSummary = forwardRef(
 
     return (
       <Box sx={{ pt: 1 }}>
-        {/* <PaymentFailChecker /> */}
+        <PaymentFailChecker />
         <Box sx={header}>{t.orderSummary}</Box>
         {/* products price */}
         <Box className="d-flex justify-content-between mb-2">
