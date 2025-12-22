ad850aa813ab8a00a28f0670a9ea2b498c03b871
michealabidfekrysaad <michealabidfekry@gmail.com>
2025-12-22 16:15:41 +0200

    enahnce express delivery label

--- components/expressDelivery/index.js
+++ components/expressDelivery/index.js
@@ -3,10 +3,12 @@ import React from "react";
 import style from "../shared/ProductCard/ProductCard.module.scss";
 import Image from "next/image";
 import useLocalization from "@/config/hooks/useLocalization";
+import useScreenSize from "@/constants/screenSize/useScreenSize";
 
 // 'case expressDelivery = 'express-delivery
 function ExpressDelivery({ text }) {
   const { t } = useLocalization();
+  const { isMobile } = useScreenSize();
 
   return (
     <Box
@@ -16,17 +18,17 @@ function ExpressDelivery({ text }) {
         position: "relative",
         left: "0px",
         right: "0px",
-        fontSize: "13px",
+        fontSize: isMobile ? "10px" : "13px",
       }}
       className={`${style["prod-label"]}`}
     >
       <Image
         src="/icons/express-delivery.svg"
         alt="express"
-        width={20}
-        height={20}
+        width={isMobile ? 15 : 20}
+        height={isMobile ? 15 : 20}
         style={{
-          marginInlineEnd: "5px",
+          marginInlineEnd: isMobile ? "3px" : "5px",
         }}
       />
       {text || t.expressDilvery}
