9110123937c4b1dc608e5001eb431d3701f18a21
michealabidfekrysaad <michealabidfekry@gmail.com>
2026-01-21 16:18:01 +0200

    add refetch interval to check for payment failed in car pricing

--- pages/carPricing/checkout/CarPricingCheckoutSummary.js
+++ pages/carPricing/checkout/CarPricingCheckoutSummary.js
@@ -236,6 +236,7 @@ const CarPricingCheckoutSummary = forwardRef(
       url: `${VEHICLE_PRICING_ORDERS}${CALCULATE_RECEIPT}`,
       method: "post",
       refetchOnWindowFocus: true,
+      refetchInterval: 5000,
       body: {
         promo_code_id: allPromoCodeData?.id || null,
       },
